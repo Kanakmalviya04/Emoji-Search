54d335d5b3ef1a53d38b879e329dfc9144bb0e1d
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2022-10-01 20:35:20 +0530

    Emoji-Searcher Added ☺

---
+++ src/component/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return(
+        <nav>
+        <h2>Emojee</h2>
+         <a href="https://github.com/Kanakmalviya04/Emoji-Search" target="_blank" rel="noreferrer">Github</a>
+         </nav>
+    );
+}
+
+export default Nav;
